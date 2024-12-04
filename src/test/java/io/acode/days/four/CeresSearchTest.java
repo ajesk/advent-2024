98a5be980f23768e388c14ed0f1eedc68254d6a7
Aaron Jeskie <aaronjeskie@gmail.com>
2024-12-04 16:54:54 -0500

    day4 yay

---
+++ src/test/java/io/acode/days/four/CeresSearchTest.java
@@ -0,0 +1,40 @@
+package io.acode.days.four;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CeresSearchTest {
+
+    @Test
+    void validateTestData() {
+        CeresSearch target = new CeresSearch("four/day4test.txt");
+
+        var result = target.findXmas();
+        assertEquals(18, result);
+    }
+
+    @Test
+    void validateFirstProblem() {
+        CeresSearch target = new CeresSearch("four/day4.txt");
+
+        var result = target.findXmas();
+        assertEquals(2639, result);
+    }
+
+    @Test
+    void validateTestDataPart2() {
+        CeresSearch target = new CeresSearch("four/day4test.txt");
+
+        var result = target.findMasX();
+        assertEquals(9, result);
+    }
+
+    @Test
+    void validateFirstProblemPart2() {
+        CeresSearch target = new CeresSearch("four/day4.txt");
+
+        var result = target.findMasX();
+        assertEquals(2005, result);
+    }
+}
